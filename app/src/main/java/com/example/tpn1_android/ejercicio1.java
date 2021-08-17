b3d5efa2384af8b58bbc2c8d3d9022423dfabf5d
Juan <40309961+JuanGonzalez99@users.noreply.github.com>
2021-08-16 21:25:03 -0300

    funcionalidad activity 1

--- app/src/main/java/com/example/tpn1_android/ejercicio1.java
+++ app/src/main/java/com/example/tpn1_android/ejercicio1.java
@@ -4,20 +4,35 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ejercicio1 extends AppCompatActivity {
 
     private EditText numero1;
+    private EditText numero2;
+    private TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio1);
+
         numero1 = findViewById(R.id.txtPrimerNumero);
+        numero2 = findViewById(R.id.txtSegundoNumero);
+        resultado = findViewById(R.id.tvResultado);
 
         //Boton ← del ActionBar
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void calcular(View view) {
+        Integer num1 = Integer.parseInt(numero1.getText().toString());
+        Integer num2 = Integer.parseInt(numero2.getText().toString());
+
+        String res = String.valueOf(num1 + num2);
+        resultado.setText(res);
     }
 }
