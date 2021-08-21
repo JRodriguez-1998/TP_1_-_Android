f8c6e36cb724cdfcc1d8c5b325767af27f4977df
Juan <40309961+JuanGonzalez99@users.noreply.github.com>
2021-08-20 22:13:20 -0300

    fix splash

---
+++ app/src/main/java/com/example/tpn1_android/splash.java
@@ -0,0 +1,28 @@
+package com.example.tpn1_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent i = new Intent(splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea,2000);
+    }
+}
